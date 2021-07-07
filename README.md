6b998dba0e071ed56d4333be92307bfbb365f26e
EconomiaItalia <82286837+EconomiaItalia@users.noreply.github.com>
2021-07-07 04:19:31 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Quanto è lungo il tuo libbberale?
-Il quiz quanto è lungo il tuo libbberale ti permette di accedere alla patente di libbberale vero, a prescindere dal risultato. Il primo test al mondo che non discrimina nessuno, a prescindere dalla lunghezza delle varie componenti. Che aspetti? Affrettati anche tu a misurarti e ottenere il vero risonoscimento coem membro del mondo che ti circonda
+SapplyValues is a clone of the Sapply political compass, made to use 8values' UI.
---
+++ results.js
@@ -0,0 +1,14 @@
+results = [
+  {"name": "Dino Parrano", "image": "./img/Dino Parrano.svg", "x_axis": 3.38, "y_axis": -5.13},
+  {"name": "Costantino De Blasi", "image": "./img/Costantino De Blasi.svg", "x_axis": 5.67, "y_axis": -1.15},
+  {"name": "Entropy for Life", "image": "./img/Entropy for life.svg", "x_axis": 3, "y_axis": -1.54},
+  {"name": "Lucio Di Gaetano", "image": "./img/Lucio Di Gaetano.svg", "x_axis": 7.33, "y_axis": -6.54},
+  {"name": "Onorevole berti", "image": "./img/on Berti.svg", "x_axis": 3, "y_axis": -1.92},
+  {"name": "Marco Cartasegna", "image": "./img/Marco Cartasegna.svg", "x_axis": 5, "y_axis": 0.38},
+  {"name": "C'è di peggio", "image": "./img/C'è di peggio.svg", "x_axis": 6.33, "y_axis": -2.31},
+  {"name": "Gaia e Pietro", "image": "./img/Gaia Zorzi.svg", "x_axis": 4, "y_axis": -2.69},
+  {"name": "Rick du Fer", "image": "./img/Rick DuFer.svg", "x_axis": 5.33, "y_axis": -3.85},
+  {"name": "Boban Pesov", "image": "./img/Bobanpesov.svg", "x_axis": 1.25, "y_axis": -1.67},
+  {"name": "Andrea Lorenzon", "image": "./img/Lorenzon.svg", "x_axis": -1.33, "y_axis": 2.69},
+  {"name": "Carlo Stagnaro", "image": "./img/Carlo Stagnaro.svg", "x_axis": 5.83, "y_axis": -6.67}
+];
